4e43988c9bf3b0a942c1f4ac81833152501eaa15
Ni <nisargedu2015@gmail.com>
2025-07-12 12:19:23 -0700

    updated demo2

--- demo2.py
+++ demo2.py
@@ -53,16 +53,16 @@ def demo(args):
     # right_images = np.load(args.right_imgs) #sorted(glob.glob(args.right_imgs, recursive=True))
     
     
-    if left_h5_file and right_h5_file:
+    if args.left_h5_file and args.right_h5_file:
         try:
-            with h5py.File(left_h5_file, 'r') as f:
+            with h5py.File(args.left_h5_file, 'r') as f:
                 left_all = f['data'][()]   # or np.array(f['left'])
-            with h5py.File(right_h5_file, 'r') as f:
+            with h5py.File(args.right_h5_file, 'r') as f:
                 right_all = f['data'][()]
         except Exception as e:            
-            with h5py.File(left_h5_file, 'r') as f:
+            with h5py.File(args.left_h5_file, 'r') as f:
                 left_all = f['left'][()]   # or np.array(f['left'])
-            with h5py.File(right_h5_file, 'r') as f:
+            with h5py.File(args.right_h5_file, 'r') as f:
                 right_all = f['right'][()]
       
         print(left_all.shape, right_all.shape)
